ffcb3dfdce07f0ed860a1471615b1649ffb6475b
RahulBishnoi505 <rahulbishnoi505@gmail.com>
2023-06-23 15:43:52 +0530

    implemented the openai API

--- website/views.py
+++ website/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.contrib import messages
+import openai
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 # Create your views here.
 
@@ -13,5 +18,27 @@ def home(request):
         if language == "Programming Language":
             messages.success(request, 'Please select a programming language')
             return render(request, "website/index.html", {"language_list":language_list, "code":code, "language": language})
-        return render(request, "website/index.html", {"language_list":language_list, "code":code, "language": language})
+        else:
+            #OpenAI Key
+            openai.api_key = os.environ.get("API_KEY")
+            #OpenAI 
+            openai.Model.list()
+            # Make an OpenAI Request
+            try:
+                response = openai.Completion.create(
+                    engine = 'text-davinci-003',
+                    prompt = f'Respond with code only. Nothing else than code. Fix this {language} code: {code} and return only the fixed code.',
+                    temperature = 0,
+                    max_tokens = 1000,
+                    top_p = 1.0,
+                    frequency_penalty = 0.0,
+                    presence_penalty = 0.0
+                    )
+                response = response["choices"][0]["text"].strip()
+                print(response)
+                return render(request, "website/index.html", {"language_list":language_list, "response": response, "language": language})
+            except Exception as e:
+                return render(request, "website/index.html", {"language_list":language_list, "code": e, "language": language})
+
+
     return render(request, "website/index.html", {"language_list":language_list})
